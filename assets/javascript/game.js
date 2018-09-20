1d0589e5df9d92938b7e8c7daad15293d358c885
Renee Wysocki <reneew311@gmail.com>
2018-09-20 12:44:18 -0500

    question functionality

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,9 @@
 $(document).ready(function () {
-    questionCounter = 0;
+    var questionCounter = 0;
     var timer = 30;
+    var correct = 0;
+    var incorrect = 0;
+    var unanswered = 0;
 
     var questions = [
         {
@@ -23,18 +26,61 @@ $(document).ready(function () {
         $(".mainArea").html(startScreen);
     }
 
-   startButton();
+    startButton();
 
-   //gives start button functionality
-   $("body").on("click", ".start-button", function(event){
-    generateHTML();
-    
-   });
+    //gives start button functionality
+    $("body").on("click", ".start-button", function (event) {
+        generateHTML();
 
-   //generates the question
-   function generateHTML() {
-	gameHTML = "<p class='text-center timerP'>Time: <span class='timer'> 30 </span></p><p class='hintImageP'>" + questions[questionCounter].imageUrl + "</p><p class='text-left questionP'>"  + questions[questionCounter].question + "</p><p class='answer'>A. " + questions[questionCounter].choices[0] + "</p><p class='answer'>B. "+ questions[questionCounter].choices[1] +"</p><p class='answer'>C. "+ questions[questionCounter].choices[2] +"</p><p class='answer'>D. "+ questions[questionCounter].choices[3] +"</p>";
-    $(".mainArea").html(gameHTML);
-}
+    });
+
+    //generates the question
+    function generateHTML() {
+        gameHTML = "<p class='text-center timerP'>Time: <span class='timer'> 30 </span></p><p class='hintImageP'>" + questions[questionCounter].imageUrl + "</p><p class='text-left questionP'>" + questions[questionCounter].question + "</p><p class='answer'>" + questions[questionCounter].choices[0] + "</p><p class='answer'>" + questions[questionCounter].choices[1] + "</p><p class='answer'>" + questions[questionCounter].choices[2] + "</p><p class='answer'>" + questions[questionCounter].choices[3] + "</p>";
+        $(".mainArea").html(gameHTML);
+    }
+
+    //allows answers to be clicked and compared.
+    $("body").on("click", ".answer", function (event) {
+        //answeredQuestion = true;
+        var selectedAnswer = $(this).text();
+        //console.log(selectedAnswer);
+        //console.log(questions[questionCounter].correctAnswer);
+        if (selectedAnswer === questions[questionCounter].correctAnswer) {
+            // console.log("Correct Answer");
+            generateWin();
+        }
+        else {
+            //console.log("Incorrect Answer");
+            generateLoss();
+        }
+    });
+
+    function generateWin() {
+        correct++;
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timer + "</span></p>" + "<p class='text-center'>Correct! The answer is: " + questions[questionCounter].correctAnswer + "</p>" + questions[questionCounter].imageUrl;
+        $(".mainArea").html(gameHTML);
+        setTimeout(wait, 3000);
+    };
+
+    function generateLoss() {
+        gameHTML = "<p class='text-center timer-p'>Time Remaining: <span class='timer'>" + timer + "</span></p>" + "<p class='text-center'>Wrong! The correct answer is: " + questions[questionCounter].correctAnswer + "</p>" + "<img class='center-block img-wrong' src='assets/images/ramsey/are-you-kidding-me.gif'>";
+        $(".mainArea").html(gameHTML);
+        setTimeout(wait, 3000);
+        incorrect++;
+    };
+
+    function wait() {
+        if (questionCounter < questions.length) {
+        questionCounter++;
+        generateHTML();
+        counter = 30;
+        //timerWrapper();
+        }
+        else {
+         //   finalScreen();
+         console.log("end")
+        }
+    }
 
 });
